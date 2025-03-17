41717036576d5717e0abe9d44abbe6ae4974335d
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-03-17 14:10:30 +0100

    manual classification

--- emails.R
+++ emails.R
@@ -13,12 +13,41 @@ email_pkg_dt <- pkg_dt[, nc::capture_all_str(
   email=list('[^">]+@', domain='.*?'),
   '[">]'),
   by=Package]
-domains <- c(
+
+commercial_patterns <- c(
   "novartis", "roche", "google", "amazon", "facebook", "apple", 
-  "microsoft", "jpmorganchase", "goldmansachs", "bankofamerica", 
-  "morganstanley", "citigroup", "barclays", "wellsfargo", "bnpparibas", 
-  "db", "jefferies")
-domain_pat <- paste0("(", paste(domains, collapse="|"), ")[.]")
+  "microsoft",
+  "iddb", "appsilon", "certara", "merck", "poissonconsulting",
+  "gene", "cynkra", "thinkr", "gsk", "windsor", "openanalytics",
+  "erasmusmc", "atorusresearch", "humanpredictions", "pfizer", "ixpantia",
+  "boehringer-ingelheim", "business-science", "lilly", "astrazeneca",
+  "sanofi", "dreamrs", "rand", "win-vector", "civisanalytics", "novonordisk",
+  "symbolix", "ardata", "westat", "4dscape", "mango-solutions",
+  "bnosac", "abbvie", "epiconcept", "for-cast", "h2o", "insight-rx",
+  "sunholo", "crunch", "analythium", "biogen", "eworkx", "occams",
+  "mirai-solutions", "bayer", "rte-france", "curso-r", "bms", "amgen",
+  "medtronic", "sevenbridges", "velsera", "menne-biomed", "databricks",
+  "aggregate-genius", "peak.ai", "rconis.com", "walker-data.com",
+  "metrumrg.com", "tidy-intelligence.com", "datastorm.fr", "utiligize.com",
+  "fromdatatowisdom.com", "helgasoft.com",
+  ##"arjohnsonau.com",
+  "ibacon.com",
+  ##"flowervalleyconsulting.com",
+  ##"statistikkonsult.com",
+  "illumina.com",
+  "gradientmetrics.com",
+  "softwareag.com",
+  "beckman.com",
+  "jumpingrivers.com",
+  "tillotts.com",
+  "zarathu.com",
+  "canva.com",
+  "experis.com",
+  "wlogsolutions.com",
+  "fellstat.com",
+  "lixoft.com",
+  "quantil")
+domain_pat <- paste0("(", paste(commercial_patterns, collapse="|"), ")[.]")
 company_dt <- email_pkg_dt[
   grep(domain_pat, domain)
 ][order(domain, email)]
@@ -27,3 +56,15 @@ company_dt <- email_pkg_dt[
   emails=length(unique(email))
 ), by=domain][order(-emails, -packages)])
 fwrite(company_dt[, .(email,Package)], "emails.csv")
+
+domains_dt <- email_pkg_dt[, .(
+  count=.N
+), by=domain
+][
+  order(-count)
+][
+, commercial := ifelse(count >= 4, 0, NA)
+][
+  company_dt, commercial := 1, on="domain"
+][]
+fwrite(domains_dt, "domains.csv")
